f8f60ae33ea6137c6990aa260d354287fbc14b12
boych <vitaliibo90@gmail.com>
2023-04-13 12:12:28 +0300

    Removing redundant imports

--- my-app/src/routes/register.tsx
+++ my-app/src/routes/register.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import logo from '../logo.svg';
-import { Counter } from '../features/counter/Counter';
-import { Header } from '../features/header/Header';
 import '../App.css';
 
 function Register() {
